78d99c5a4017cf416c48259b992a497dfe229301
Luis Alberto Martínez Calderon <mm6704924@gmail.com>
2025-12-16 02:16:02 -0500

    up

--- MundoDonghuaProvider/src/main/kotlin/com/stormunblessed/MundoDonghua.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/stormunblessed/MundoDonghua.kt
@@ -2,16 +2,14 @@ package com.stormunblessed
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.extractors.Dailymotion
-import com.lagradost.cloudstream3.extractors.Okrulink
-import com.lagradost.cloudstream3.extractors.FileMoonSx
 
 @CloudstreamPlugin
-class stormunblessed: BasePlugin() {
+class MundoDonghuaPlugin : BasePlugin() {
     override fun load() {
-        registerMainAPI(stormunblessed())
-        registerExtractorAPI(FileMoonSx())
-        registerExtractorAPI(Dailymotion())
-        registerExtractorAPI(Okrulink())
+        registerMainAPI(MundoDonghuaProvider())
+        // Si quieres registrar extractores adicionales:
+        // registerExtractorAPI(FileMoonSx())
+        // registerExtractorAPI(Dailymotion())
+        // registerExtractorAPI(Okrulink())
     }
 }
