b59a5e70dc39596e3577e2b2b35faaadde19742e
Luis Alberto Martínez Calderon <46967629+LuisMartinezCalderon@users.noreply.github.com>
2025-12-15 19:55:15 -0500

    Build 2ac2b593fd519d7593afd5c5c1c7eb6612bef100

---
+++ MundoDonghuaProvider/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.mundodonghua"
+
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+        // 🚫 Sin versionCode / versionName (solo para aplicaciones)
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    // API de CloudStream
+    implementation("com.lagradost:cloudstream3:3.6.2")
+
+    // Kotlin stdlib
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+}
