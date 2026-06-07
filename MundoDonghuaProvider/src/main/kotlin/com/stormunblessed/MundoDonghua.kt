e95c24a12b68267028f3a43bbe31a0fd805ece0a
Ubuntu <azureuser@myVm.y3pepptnxcdebdjd1asyvshqrb.gvxx.internal.cloudapp.net>
2026-06-07 18:51:45 +0000

    up 9.1.0.7

--- MundoDonghuaProvider/src/main/kotlin/com/stormunblessed/MundoDonghua.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/stormunblessed/MundoDonghua.kt
@@ -59,7 +59,7 @@ class MundoDonghuaProvider : MainAPI() {
 
         return if (epsAnchor.size > 1) {
             val episodes: List<Episode>? = epsAnchor.map {
-                val epPoster = it.select("img").attr("src")
+                val epPoster = it.select("img").attr("lazy-src")
                 val epHref   = it.attr("href")
 
                 newEpisode(epHref) {
