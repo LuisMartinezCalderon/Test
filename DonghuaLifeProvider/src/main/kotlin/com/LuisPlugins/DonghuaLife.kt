4b41a7c106c108ce76431cc8f18d9fad71209c69
Ubuntu <azureuser@myVm.y3pepptnxcdebdjd1asyvshqrb.gvxx.internal.cloudapp.net>
2026-06-09 20:05:33 +0000

    up 9.1.43

--- DonghuaLifeProvider/src/main/kotlin/com/LuisPlugins/DonghuaLife.kt
+++ DonghuaLifeProvider/src/main/kotlin/com/LuisPlugins/DonghuaLife.kt
@@ -26,7 +26,8 @@ class DonghuaLifeProvider : MainAPI() {
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val url = request.data + page
         val document = app.get(url).document
-        val home = document.select(".view-donghuas .serie").mapNotNull { it.animeFromElement() }
+       val home = document.select(".view-donghuas .serie").mapNotNull { it.animeFromElement() }
+        
 
         return newHomePageResponse(
                 list = HomePageList(name = request.name, list = home, isHorizontalImages = false),
