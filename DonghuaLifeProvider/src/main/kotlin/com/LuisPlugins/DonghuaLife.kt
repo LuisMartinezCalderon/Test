c1e90064ecc4ac20c6b8bb0b8376342ecaaef430
Ubuntu <azureuser@myVm.y3pepptnxcdebdjd1asyvshqrb.gvxx.internal.cloudapp.net>
2026-06-10 02:52:31 +0000

    up 9.1.61

--- DonghuaLifeProvider/src/main/kotlin/com/LuisPlugins/DonghuaLife.kt
+++ DonghuaLifeProvider/src/main/kotlin/com/LuisPlugins/DonghuaLife.kt
@@ -26,7 +26,7 @@ class DonghuaLifeProvider : MainAPI() {
         // 1. Siempre construimos la URL con el parámetro ?page=
         // Asumimos que el primer parámetro es page-1, así page=1 se convierte en ?page=0, page=2 en
         // ?page=1, etc.
-          val url = request.data + page
+          val url = request.data + {page - 1}
        // val url = "$mainUrl${page - 1}"
       //  "$mainUrl/donghuas?page=${page - 1}"
 
