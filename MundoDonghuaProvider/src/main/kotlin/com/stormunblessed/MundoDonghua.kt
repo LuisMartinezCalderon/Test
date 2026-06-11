b9ca2f93919b98901dba6795d3f00025103803f0
Ubuntu <azureuser@myVm.y3pepptnxcdebdjd1asyvshqrb.gvxx.internal.cloudapp.net>
2026-06-11 18:33:52 +0000

    up 9.1.77

--- MundoDonghuaProvider/src/main/kotlin/com/stormunblessed/MundoDonghua.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/stormunblessed/MundoDonghua.kt
@@ -28,7 +28,7 @@ class MundoDonghuaProvider : MainAPI() {
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val url = request.data + page
-        val document = app.get(url,headers = headers, interceptor = CloudflareKiller()).document
+        val document = app.get(url,headers = headers,).document
         val home = document.select("a[href*='/donghua/']").mapNotNull { it.animeFromElement("h3") }
 
         return newHomePageResponse(
